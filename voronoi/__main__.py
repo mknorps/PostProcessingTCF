7e88d0da48ac9ff82b3fd188d1a01310e8456122
Maria Knorps <maria.knorps@gmail.com>
2018-03-02 18:58:24 +0100

    docstring added

--- voronoi/__main__.py
+++ voronoi/__main__.py
@@ -13,25 +13,72 @@ import matplotlib.pyplot as plt
 
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
-from pplib import options
 from pplib import halo 
 
 
-def draw_histogram(pict_path,x,theoretical,*args_experiment):
+def draw_histogram(pict_path, bc, *args_experiment, theoretical = None):
+    '''
+    Draw histogram and save it as pict_path
+
+    Parameters
+    ----------
+    pict_path : str
+        file name to save plot
+    bc : list of floats
+        bin centers
+    theoretical: array(float), optional 
+        theoretical distribution
+    args_experiment: list
+        distribution obtained in experiment
+        single element is a dictionary:
+        {'data':array(float), 'label':str}
+
+    Output
+    ------
+    Figure of histogram saved in pict_path    
+
+    '''
+
+    for arg in args_experiment:
+        assert len(arg['data']) == len(bc)
 
     fig = plt.figure(figsize = (6,4))
-    ax1 = plt.subplot2grid((1,1),(0,0)) #tme_xx
+    ax1 = plt.subplot2grid((1,1),(0,0))
 
     ax1.set_xlabel("$A/\langle A\\rangle$",fontsize=15)
 
-    ax1.plot(x,theoretical,label="$\Gamma(a,b)$", color='black')
+    ax1.plot(bc,theoretical,label="$\Gamma(a,b)$", color='black')
     for arg in args_experiment:
-        ax1.plot(x,arg['data'],label=arg['label'])
+        ax1.plot(bc,arg['data'],label=arg['label'])
 
     leg = ax1.legend(fontsize=15)
-    plt.tight_layout()#
-    fig.savefig(pict_path )
+    plt.tight_layout()
+    fig.savefig(pict_path)
     plt.close(fig)
+
+
+def draw_voronoi_from_slice(input_file, pict_path, usecols=["x","z"]):
+    '''
+    Draw voronoi polygons and save it as pict_path
+
+    Parameters
+    ----------
+    pict_path : str
+        file name to save plot
+    points : array
+        
+    Output
+    ------
+    Graphical interpretation of Voronoitessalation
+    saved in pict_path    
+
+    '''
+    points = pd.read_csv(input_file, usecols=usecols)
+    vor = Voronoi(points)
+    voronoi_plot_2d(vor,show_vertices=False,point_size=1)
+    
+    plt.savefig(pict_path)
+
 
 def run_project(args):
     
@@ -39,23 +86,9 @@ def run_project(args):
 
     parsed_args = opt.parse(args[1:])
 
-    points = pd.read_csv("/home/gemusia/results_for_PhD/voronoi/slices/LES/slice_LES_St25_4860_left.csv",usecols=["x","z"])
-    #vor = Voronoi(points)
-    #plt.ion()
-    #voronoi_plot_2d(vor,show_vertices=False,point_size=1)
-    #plt.hold(True)
-
-    df = halo.points_with_halo(points,'x','z',0.1)
- 
-    print(df.head())
-    print(df.describe())
-
-
-    #vor = Voronoi(df)
-    #voronoi_plot_2d(vor,show_vertices=False,point_size=3)
-    #plt.show()
-
-    #plt.hold(False)
+    slice_file = "/home/gemusia/results_for_PhD/voronoi/slices/LES/slice_LES_St25_4860_left.csv"
+    pict_path = "/home/gemusia/results_for_PhD/voronoi/slices/LES/voronoi_snapshot_LES_St25.eps"
+    draw_voronoi_from_slice(slice_file, pict_path)
 
     data_St1 = np.loadtxt("/home/gemusia/kody/voronoi_IMP/hist_FRACTAL_St1.res",skiprows=5)
     data_St5 = np.loadtxt("/home/gemusia/kody/voronoi_IMP/hist_FRACTAL_St5.res",skiprows=5)
